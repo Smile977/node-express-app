fc49de3022ee748bfcad2369aeddb96bc24f4536
Andrei Biareshchanka <andrei_biareshchanka@epam.com>
2021-03-25 11:03:59 +0300

    add routes and views files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes/routes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(router);
+app.use((req, res, next) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, 'views', 'error.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port: ${PORT}...`);
+})
+
+
