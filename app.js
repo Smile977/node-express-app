f7175c96349a9ce124220ac0d1486a44ccfeca5e
Andrei Biareshchanka <andrei_biareshchanka@epam.com>
2021-03-26 10:06:19 +0300

    add json from send to front

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes/routes');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(router);
+app.use('/todos', router);
 app.use((req, res, next) => {
   res.status(404);
   res.sendFile(path.join(__dirname, 'views', 'error.html'));
--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,12 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views', 'index.html'));
-});
-
-router.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views', 'about.html'));
-});
+    res.json([
+      {id: 1, title: 'Second todo', complited: true},
+      {id: 2, title: 'One more todo', complited: true},
+      {id: 3, title: 'The next todo', complited: false},
+      {id: 4, title: 'Last todo', complited: false},
+    ]);
+  });
 
 module.exports = router;
